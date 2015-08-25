e34061bbe17d77da5896470baf7f2429a01caa5d
jschirrmacher <j.schirrmacher@justso.de>
2015-08-25 21:10:02 +0200

    Don't log in user if activation is sent

--- PasswordAuthenticator.php
+++ PasswordAuthenticator.php
@@ -21,6 +21,7 @@ class PasswordAuthenticator extends Authenticator
         $password = $request->getParam('password', '', true);
         if ($password === '') {
             $this->needsActivation = true;
+            $this->user = null;
         } else if ($this->user->checkPassword($password)) {
             $this->newUser = false;
         } else {
